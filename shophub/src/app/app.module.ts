f4ea349314c7a045fd8433c611ebd8a8b7a1578d
swaahire <44641075+swaahire@users.noreply.github.com>
2019-02-15 16:09:23 +0530

    angular project

---
+++ shophub/src/app/app.module.ts
@@ -0,0 +1,25 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+// import { FooterComponent } from './footer/footer.component';
+// import { LayoutComponent } from './layout/layout.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    HeaderComponent,
+    // FooterComponent,
+    // LayoutComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  // exports: [LayoutComponent],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
